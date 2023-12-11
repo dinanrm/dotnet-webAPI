97786110e8c27191037bda0a653509d2b323ba0f
Dinan Rangga Maulana <dinansemail@gmail.com>
2023-12-11 14:50:35 +0700

    Add WebAPI .Net v8 - MongoDB

---
+++ .Net8/BookStoreApi/BookStoreApi/Controllers/BooksController.cs
@@ -0,0 +1,75 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _booksService;
+
+        public BooksController(BooksService booksService) =>
+            _booksService = booksService;
+
+        [HttpGet]
+        public async Task<List<Book>> Get() =>
+            await _booksService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            await _booksService.CreateAsync(newBook);
+
+            return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Book updatedBook)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            updatedBook.Id = book.Id;
+
+            await _booksService.UpdateAsync(id, updatedBook);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            await _booksService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
